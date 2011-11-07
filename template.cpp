84771401573f53eb06f72e5926025576f7850655
Luke Seelenbinder <luke.seelenbinder@gmail.com>
2011-11-07 14:05:51 -0500

    Add template file.

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+#include <stack>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
